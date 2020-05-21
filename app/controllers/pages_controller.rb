2c86d9a4816e062508514e49663f0e422e426fd9
Brad Fujimoto <bradjordan@gmail.com>
2020-05-21 12:46:15 +0200

    cleaning done, created dashboard with 3 functionalities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @my_pets = current_user.pets
+    @my_bookings = current_user.bookings
+    @client_bookings = current_user.client_bookings
+  end
 end
