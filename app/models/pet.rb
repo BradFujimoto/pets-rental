ae5d44a9e6ccd59637ca4b885a21b288eae03212
Myriam Dubois <myriam.dubois@skema.edu>
2020-05-21 15:02:35 +0200

    Validations on pet added and seed updated

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,13 @@
 class Pet < ApplicationRecord
+  SPECIES = [ "Cat", "Dog", "Meerkat", "Spider", "Elephant", "Mouse", "Parrot", 'Wolf','Panda', 'Scorpion', 'Lion', 'Iguane']
+  ENVIRONMENT = [ "Indoor", "Outdoor", "Both"]
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  validates :name, presence: true
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :species, presence: true, inclusion: { in: SPECIES  }
+  validates :environment, presence: true, inclusion: { in: ENVIRONMENT  }
+  validates :price, presence: true
 end
